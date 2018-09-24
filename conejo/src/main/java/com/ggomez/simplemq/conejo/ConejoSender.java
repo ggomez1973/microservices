9a0339c807cd45f42d056f70cba23fa81ab753cb
German <g.gomez@globallogic.com>
2018-09-24 14:56:25 -0300

    Simple sender/reciever con RabbitMQ y Spring AMQP

---
+++ conejo/src/main/java/com/ggomez/simplemq/conejo/ConejoSender.java
@@ -0,0 +1,21 @@
+package com.ggomez.simplemq.conejo;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class ConejoSender {
+    @Autowired
+    private RabbitTemplate template;
+
+    @Autowired
+    private Queue queue;
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 500)
+    public void enviar() {
+        String message = "Mensaje simple!";
+        this.template.convertAndSend(queue.getName(), message);
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+}
---
+++ conejo/src/test/java/com/ggomez/simplemq/conejo/ConejoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ggomez.simplemq.conejo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConejoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
