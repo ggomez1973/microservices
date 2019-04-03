a5d91c795c15050e6df079212b557f6c4cb012f1
ggomez1973 <g.gomez@globallogic.com>
2019-04-03 18:14:45 -0300

    All 3 services UP

--- triplepack/cable/src/main/java/com/triplepack/cable/CableApplication.java
+++ triplepack/cable/src/main/java/com/triplepack/cable/CableApplication.java
@@ -2,14 +2,20 @@ package com.triplepack.cable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 //@EnableCircuitBreaker
 public class CableApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CableApplication.class, args);
 	}
 }
+
+
---
+++ triplepack/cable/src/main/java/com/triplepack/cable/commands/GreetingController.java
@@ -0,0 +1,17 @@
+package com.triplepack.cable.commands;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+class GreetingController {
+    private static final Logger LOG = Logger.getLogger(GreetingController.class.getName());
+
+    @RequestMapping("/")
+    public String getGreeting(){
+        LOG.info("La concha de tu hermana All Boys");
+        return "Sarasa!!!";
+    }
+}
