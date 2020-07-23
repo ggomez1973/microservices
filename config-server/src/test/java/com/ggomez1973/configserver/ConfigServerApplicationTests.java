be805b8716c481d096479e0b925a1a0aff48077e
German Gomez <ggomez1973@gmail.com>
2020-07-23 19:55:07 -0300

    minor changes

---
+++ config-server/src/test/java/com/ggomez1973/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggomez1973.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
