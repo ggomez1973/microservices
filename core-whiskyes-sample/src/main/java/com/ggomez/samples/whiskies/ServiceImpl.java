1e7e7af43656f84a04954fa1105e541260736c01
German Gomez <ggomez1973@gmail.com>
2021-01-26 20:53:49 -0300

    INITIAL COMMIT

---
+++ core-whiskyes-sample/src/main/java/com/ggomez/samples/whiskies/ServiceImpl.java
@@ -0,0 +1,2 @@
+package com.ggomez.samples.whiskies;public class ServiceImpl {
+}
---
+++ endava/saga_sample/saga-config-server/src/test/java/com/endava/training/sagaconfigserver/SagaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.training.sagaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
